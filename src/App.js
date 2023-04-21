3d64b849c97565d3a7189e3306dd4f39280c8454
Stephane <horlogerie@alabonneheure.fr>
2023-04-21 17:30:26 +0300

    dynamic tab name

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
@@ -17,6 +20,19 @@ import Site from './components/Site/Site'
 import './App.css';
 
 function App() {
+  const [tabsList, setTabsList] = useState([])
+  useEffect(() => {
+    axios.get('https://bo-ecommerce-f3e6bf.appdrag.site/api/getTabs', {
+      params: {
+        "AD_PageNbr": "1",
+        "AD_PageSize": "500"
+      }
+    })
+      .then((response) => {
+        setTabsList(response.data.Table)
+      });
+  }, [])
+
   return (
     <div className="header">
       <h3>monsite.fr</h3>
@@ -26,30 +42,23 @@ function App() {
         className="m-4"
         style={{ fontSize: '14px', lineHeight: '15px' }}
       >
-        <Tab eventKey="home" tabClassName="home tab-spacing" title="HOME" >
-          <Home />
-        </Tab>
-        <Tab eventKey="products" tabClassName="products tab-spacing" title="PRODUCTS">
-          <Products />
-        </Tab>
-        <Tab eventKey="customers" tabClassName="customers tab-spacing" title="CUSTOMERS">
-          <Customers />
-        </Tab>
-        <Tab eventKey="orders" tabClassName="orders tab-spacing" title="ORDERS">
-          <Orders />
-        </Tab>
-        <Tab eventKey="articles" tabClassName="articles tab-spacing" title="ARTICLES">
-          <Articles />
-        </Tab>
-        <Tab eventKey="settings" tabClassName="settings tab-spacing" title="SETTINGS">
-          <Settings />
-        </Tab>
-        <Tab eventKey="statistics" tabClassName="statistics tab-spacing" title="STATISTICS">
-          <Statistics />
-        </Tab>
-        <Tab eventKey="site" tabClassName="site tab-spacing" title="SITE">
-          <Site />
-        </Tab>
+        {console.log({ tabsList })}
+        {
+
+          tabsList?.map((tab, i) => {
+            const Component = tab.tabName[0].toUpperCase() + tab.tabName.slice(1)
+            return (
+              <Tab
+                key={i}
+                eventKey={tab.tabName}
+                tabClassName={`${tab.tabName.toLowerCase()} tab-spacing`}
+                title={tab.tabName.toUpperCase()}
+              >
+                <Component />
+              </Tab>
+            )
+          })
+        }
         <Tab eventKey="website" tabClassName="website tab-spacing" title={<FontAwesomeIcon icon={faGlobe} />}>
         </Tab>
       </Tabs>
